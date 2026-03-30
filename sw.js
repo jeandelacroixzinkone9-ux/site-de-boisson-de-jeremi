92c69f69494688f139d9f71bafa6212a5dc9d6dd
jeandelacroixzinkone9-ux <jeandelacroixzinkone9@gmail.com>
2026-03-30 02:41:01 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
-const CACHE_NAME = "app-cache";
-
+const CACHE_NAME = 'boutique-cache-v1';
 const urlsToCache = [
-  "index.html",
-  "red-label.jpg",
-  "jack-daniel.jpg",
-  "double-black.jpg"
+  'index.html',
+  'red-label.jpg',
+  'jack-daniel.jpg',
+  'double-black.jpg',
+  'ricard.jpg',
+  'black-label.jpg',
+  'logo.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-    .then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
